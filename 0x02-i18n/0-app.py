e6fd6b80cc9c5c0e014361fbec0e3c4c82a6b6ba
EugeneJoe <jmuthui70@gmail.com>
2022-01-04 15:35:58 +0000

    Add flask app file

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    """
+    Handles / route
+    """
+    return render_template('0-index.html')
