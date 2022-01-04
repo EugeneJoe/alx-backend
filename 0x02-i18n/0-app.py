e56e654cecd45bc76593eb52bbca148aefb7e2dd
EugeneJoe <jmuthui70@gmail.com>
2022-01-04 15:47:10 +0000

    updates

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -8,9 +8,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def index():
     """
     Handles / route
     """
     return render_template('0-index.html')
+
+if __name__ = "__main__":
+    app.run(host="5000", port="0.0.0.0")
